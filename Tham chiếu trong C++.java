6ece0490e94e7bd1d17371923a84c7a8a30301a3
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-08-20 17:45:18 +0700

    Create Tham chiếu trong C++.java

---
+++ Tham chiếu trong C++.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        n = n * 28;
+        System.out.print(n);
+    }
+}
