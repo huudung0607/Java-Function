d1501eac57293bd32ef0fe3c8d09b3665dedaf10
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-08-20 17:37:47 +0700

    Create Hàm void.java

---
+++ Hàm void.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+public class Main {
+    public static void display(){
+        System.out.println("Xin chao 28tech");
+        System.out.println("Ham void");
+        System.out.println("28tech.com.vn");
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        display();
+    }
+}
